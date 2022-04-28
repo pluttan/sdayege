9fee442175623cb41d60d1f9da282467beed782d
Andrew Plutto <pluttan@ya.ru>
2022-04-28 22:12:46 +0300

    allbank

--- fipi/collector.py
+++ fipi/collector.py
@@ -2,7 +2,13 @@ import sys
 sys.path.insert(0, '/Users/pluttan/Desktop/проекты/sDAYege/sql/')
 from sql import *
 import json
+from getfileofipi import getfipi
 
+getfipi('http://ege.fipi.ru/os11/xmodules/qprint/qsearch.php?theme_guid=9485b6199541e311a27d001fc68344c9&proj_guid=AC437B34557F88EA4115D2F374B0A07B', 
+        'http://ege.fipi.ru/os11/xmodules/qprint/index.php?proj_guid=AC437B34557F88EA4115D2F374B0A07B&theme_guid=9485b6199541e311a27d001fc68344c9&md=qprint&groupno=', 
+        0, 
+        45)
+insertzadan()
 data1 = select("fipi",col_to_get=None,no_col="all")
 data2 = json.load(open('fipi/fipi.json', 'r'))
 for i in data1:
--- fipi/fipi.py
+++ fipi/fipi.py
@@ -1,4 +1,4 @@
 def passtojson(noz,z):
     z=z.replace('"',"'")
-    a=open("fipi.json").read()[:-1]
-    open("fipi.json","w").write(a+',\n    "'+noz+'" : "'+z+'"}')
+    a=open("fipi/fipi.json").read()[:-1]
+    open("fipi/fipi.json","w").write(a+',\n    "'+noz+'" : "'+z+'"}')
